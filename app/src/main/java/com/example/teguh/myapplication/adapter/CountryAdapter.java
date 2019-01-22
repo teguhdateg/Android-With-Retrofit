51faf2f913e6e01f9c0e62803c1e6fe8ab9e423e
teguh kurniawan <teguh.kurniawan2705@gmail.com>
2019-01-22 23:35:40 +0700

    Android.Retrofit

--- app/src/main/java/com/example/teguh/myapplication/adapter/CountryAdapter.java
+++ app/src/main/java/com/example/teguh/myapplication/adapter/CountryAdapter.java
@@ -1,4 +1,59 @@
 package com.example.teguh.myapplication.adapter;
 
-public class CountryAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+import com.example.teguh.myapplication.R;
+import com.example.teguh.myapplication.model.Result;
+
+import java.util.ArrayList;
+
+public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.CountryViewHolder>{
+
+    private ArrayList<Result> countriesList;
+
+    public CountryAdapter(ArrayList<Result> countriesList) {
+        this.countriesList = countriesList;
+    }
+
+    @NonNull
+    @Override
+    public CountryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+
+        LayoutInflater layoutInflater=LayoutInflater.from(parent.getContext());
+        View view=layoutInflater.inflate(R.layout.row_country,parent,false);
+
+        return new CountryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CountryViewHolder holder, int position) {
+
+        holder.countryNameTextView.setText(countriesList.get(position).getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return countriesList.size();
+    }
+
+    class CountryViewHolder extends RecyclerView.ViewHolder{
+
+
+        TextView countryNameTextView;
+
+
+        public CountryViewHolder(View itemView){
+            super(itemView);
+
+            countryNameTextView=itemView.findViewById(R.id.tv_country_name);
+        }
+    }
 }
--- app/src/main/java/com/example/teguh/myapplication/view/MainActivity.java
+++ app/src/main/java/com/example/teguh/myapplication/view/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.teguh.myapplication.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.example.teguh.myapplication.R;
+import com.example.teguh.myapplication.adapter.CountryAdapter;
 import com.example.teguh.myapplication.model.Info;
 import com.example.teguh.myapplication.model.Result;
 import com.example.teguh.myapplication.service.GetCountryDataService;
@@ -20,6 +23,8 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Result> results;
+    private CountryAdapter countryAdapter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +48,17 @@ public class MainActivity extends AppCompatActivity {
 
                     results=(ArrayList<Result>) info.getRestResponse().getResult();
 
-                    for(Result r:results){
-                        Log.i("testing123","****************************"+  r.getName());
-                    }
+//                    for(Result r:results){
+//                        Log.i("testing123","****************************"+  r.getName());
+//                    }
+
+                    viewData();
+
+
                 }
 
             }
+
 
             @Override
             public void onFailure(Call<Info> call, Throwable t) {
@@ -57,6 +67,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
         return results;
+    }
+
+    private void viewData() {
+
+        recyclerView=(RecyclerView)findViewById(R.id.rv_countries_list);
+        countryAdapter=new CountryAdapter(results);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(countryAdapter);
+
+
     }
 
 
